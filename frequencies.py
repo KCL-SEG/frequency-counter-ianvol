5eb3249da3e1a780bf75d322a8b38a6fa848082b
Ian Volkov <ian.volkov@kcl.ac.uk>
2023-09-24 18:59:32 +0100

    Solution 4

--- frequencies.py
+++ frequencies.py
@@ -8,4 +8,6 @@ def frequencies(items):
             item = str(item)
         if item in frequencies:
             frequencies[item] += 1
+        else:
+            frequencies[item] = 1
     return frequencies
